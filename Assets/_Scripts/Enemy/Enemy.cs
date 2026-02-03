aa20260cf00fed8e0fe588cb00443d2b09e7cdeb
pabloss <147928661+vhdrdnew@users.noreply.github.com>
2026-02-03 22:09:57 +0200

    add tower with target list

--- Assets/_Scripts/Enemy/Enemy.cs
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 public class Enemy : MonoBehaviour
 {
+    public int health = 50;
     [SerializeField] private float movespeed = 2f;
 
     private Rigidbody2D rb;
 
     private Transform checkpoint;
-    private int index = 0;
+    [NonSerialized]public int index = 0;
+    [NonSerialized] public float distance = 0;
 
     void Awake()
     {
@@ -23,6 +26,7 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         checkpoint = EnemyManager.main.checkpoints[index];
+        distance = Vector2.Distance(transform.position, EnemyManager.main.checkpoints[index].position);
         if (Vector2.Distance(checkpoint.transform.position, rb.transform.position) <= 0.1f)
         {
             index++;
@@ -31,11 +35,21 @@ public class Enemy : MonoBehaviour
                 Destroy(gameObject);
             }
         }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
     void FixedUpdate()
     {
         Vector2 direction = (checkpoint.position - transform.position).normalized;
         transform.right = checkpoint.position - transform.position;
         rb.linearVelocity = direction * movespeed;
+    }
+
+    public void damage(int damage)
+    {
+        health -= damage; 
     }
 }
